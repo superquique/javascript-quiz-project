046e07c6ca4500da3358cc5656e6d7c215d7d7e9
Enrique Victorero <enriquevictorero@Mac-de-Enrique.local>
2026-02-23 08:50:31 -0600

    Implemented the Quiz class

--- src/quiz.js
+++ src/quiz.js
@@ -2,14 +2,44 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
     // 2. getQuestion()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
     
     // 3. moveToNextQuestion()
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
 
     // 4. shuffleQuestions()
+    shuffleQuestions() {
+        for (let i=0; i<this.questions.length; i++) {
+            const randomIndex = Math.floor(Math.random() * this.questions.length);
+            [this.questions[i], this.questions[randomIndex]] = [this.questions[randomIndex], this.questions[i]];
+        }
+    }
 
     // 5. checkAnswer(answer)
-
+    checkAnswer(answer) {
+        if (answer === this.questions[this.currentQuestionIndex].answer) {
+            this.correctAnswers++;
+        }
+    }
     // 6. hasEnded()
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
