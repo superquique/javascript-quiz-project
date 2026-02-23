f7625270853f19d6cccfd04b8b4e36a3a1706e8e
Enrique Victorero <enriquevictorero@Mac-de-Enrique.local>
2026-02-23 08:49:43 -0600

    Implemented the Question class

--- src/question.js
+++ src/question.js
@@ -2,6 +2,19 @@ class Question {
     // YOUR CODE HERE:
     //
     // 1. constructor (text, choices, answer, difficulty)
+    constructor (text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer; //this is the correct response
+        this.difficulty = difficulty;
+    }
 
-    // 2. shuffleChoices()
+    // // 2. shuffleChoices()
+    shuffleChoices() {
+        for (let i=0; i<this.choices.length; i++) {
+            const randomIndex = Math.floor(Math.random() * this.choices.length);
+            [this.choices[i], this.choices[randomIndex]] = [this.choices[randomIndex], this.choices[i]];
+        }
+    }
+
 }
